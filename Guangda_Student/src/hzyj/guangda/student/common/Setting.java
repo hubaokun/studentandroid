3d5ac682a0fa216241bfd5634aee71840eb98b48
hubaokun@gmail.com <hubaokun@gmail.com>
2015-08-14 12:37:22 +0800

    fix baidu map's jar,comfirmactivity .class,ActivityService

--- Guangda_Student/src/hzyj/guangda/student/common/Setting.java
+++ Guangda_Student/src/hzyj/guangda/student/common/Setting.java
@@ -2,10 +2,10 @@ package hzyj.guangda.student.common;
 
 public class Setting {
 	//public static String BASE_URL = "http://192.168.1.129:8080/xiaoba/";// 测试
-	public static String BASE_URL = "http://120.25.236.228:8080/dadmin/";// 正式
+	//public static String BASE_URL = "http://120.25.236.228:8080/dadmin/";// 正式
 	//public static String BASE_URL = "http://192.168.1.117:8080/xb/";
 	//public static String BASE_URL = "http://192.168.1.114:8080/xb/";
-	//public static final String BASE_URL = "http://www.xiaobakaiche.com/dadmin/";
+	public static final String BASE_URL = "http://www.xiaobakaiche.com/dadmin/";
 	public static final String PREFS_USER_INFO = "userInfo";
 	public static final String PGY_APPID = "3211f8f555563f7bdad0713833cd3c12";//正式环境Appid
 	//public static final String PGY_APPID = "8ff9485e34fa399373487f7e65623e33";//测试环境Appid
